3deb3e493d12acd4febab0f94f9d8ac4c3360489
Rupantar Adhikary <rupantaradhikary2002@gmail.com>
2021-03-11 16:05:01 +0530

    Graph week2 complete

--- graph_problems/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
+++ graph_problems/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
@@ -4,12 +4,50 @@
 using std::vector;
 using std::pair;
 
+
+
+bool explore(vector<vector<int> > &adj, bool* visited, int curr){
+
+
+    if(visited[curr] == true){
+
+        return true;
+    }
+    visited[curr] = true;
+    for(int j = 0;j < adj[curr].size();j++){
+
+        if(explore(adj, visited, adj[curr][j])){
+            return true;
+        }
+
+    }
+
+    visited[curr] = false;
+
+    return false;
+}
+
 int acyclic(vector<vector<int> > &adj, int n) {
   //write your code here
-  bool* flags = new bool[n];
+  bool* visited = new bool[n];
   for(int i = 0;i < n;i++){
-
+    visited[i] = false;
   }
+  for(int i = 0;i < n;i++){
+    visited[i] = true;
+    for(int j = 0;j < adj[i].size();j++){
+
+        if(explore(adj, visited, adj[i][j])){
+
+            return 1;
+        }
+
+
+    }
+
+    visited[i] = false;
+  }
+
   return 0;
 }
 
@@ -22,5 +60,6 @@ int main() {
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
   }
+
   std::cout << acyclic(adj, n);
 }
--- graph_problems/week2_graph_decomposition2/2_order_of_courses/toposort.cpp
+++ graph_problems/week2_graph_decomposition2/2_order_of_courses/toposort.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include<stack>
 
 using std::vector;
 using std::pair;
+using std::stack;
+using std::cout;
 
-void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
-  //write your code here
-}     
+void explore(vector<vector<int> > &adj, stack<int>* order, bool* visited, int i){
+    for(int k = 0;k < adj[i].size();k++)
+    {
 
-vector<int> toposort(vector<vector<int> > adj) {
-  vector<int> used(adj.size(), 0);
-  vector<int> order;
+        if(!visited[adj[i][k]])
+        {
+            visited[adj[i][k]] = true;
+            explore(adj, order, visited, adj[i][k]);
+            order->push(adj[i][k]);
+        }
+
+    }
+}
+
+stack<int> toposort(vector<vector<int> > &adj, int n) {
+
+  stack<int> order;
   //write your code here
+  bool* visited = new bool[n];
+  for(int i = 0;i < n;i++)
+  {
+    visited[i] = false;
+  }
+
+  for(int i = 0;i < n;i++)
+  {
+      if(!visited[i])
+      {
+        visited[i] = true;
+        explore(adj, &order, visited, i);
+        order.push(i);
+      }
+  }
+
+
   return order;
 }
 
@@ -25,8 +55,11 @@ int main() {
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
   }
-  vector<int> order = toposort(adj);
-  for (size_t i = 0; i < order.size(); i++) {
-    std::cout << order[i] + 1 << " ";
+  stack<int> order = toposort(adj, n);
+
+  while(!order.empty())
+  {
+      cout << order.top() + 1<< " ";
+      order.pop();
   }
 }
